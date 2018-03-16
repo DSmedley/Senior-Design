efdf93d4eac19e2d1ebef1cbda962544a6ed69d6
Dillon <smedleydillon@gmail.com>
2018-03-16 11:02:57 -0400

    Query Fix

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -9,12 +9,14 @@ use DB;
 class PagesController extends Controller
 {
     public function getHome(){
-        /*$recents = Analyses::select()
-                ->distinct('screen_name')
-                ->get();*/
+        $recents = Analyses::select()
+                ->groupby('screen_name')
+                ->orderby('created_at')
+                ->limit('18')
+                ->get();
         
         //$recents = DB::select( DB::raw("SELECT DISTINCT screen_name FROM Analyses LIMIT 18") );
-        $recents = DB::select( DB::raw("SELECT *, max(created_at) FROM Analyses GROUP BY screen_name ORDER BY created_at DESC LIMIT 12") );
+        //$recents = DB::select( DB::raw("SELECT * FROM Analyses GROUP BY screen_name ORDER BY created_at DESC LIMIT 12") );
         //$recents = DB::select( DB::raw("select * from Analyses join (select screen_name, max(created_at) as created_at from Analyses group by screen_name) using (screen_name, created_at)") );
         
         return view('welcome')->with('recents', $recents);
